4019aa7db8dcc4e2616807adb63a58fde150308b
PedroNavarroGW <pedro@gerenteweb.com.br>
2023-09-26 20:28:07 -0300

    feat: create routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Router } from './Router.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global.ts'
+import { defaultTheme } from './styles/themes/default.ts'
+
 function App() {
   return (
-    <>
-    
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
